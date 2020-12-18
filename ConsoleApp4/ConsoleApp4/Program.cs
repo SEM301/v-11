c185f6672e9430a656d7f51aa24472635b04c395
SEM301 <72428570+SEM301@users.noreply.github.com>
2020-12-18 18:38:37 +0900

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("S=9,756y^7+2*tgx");
+            Random rand = new Random();
+            double x = rand.Next(0, 99);
+            double y = rand.Next(0, 99);
+            Console.WriteLine($"x={x}");
+            Console.WriteLine($"y={y}");
+            double result = Math.Tan(x);
+            double result2 = Math.Pow(y,7);
+            double S = 9.756*result2+2*result;
+            Console.WriteLine($"S={S}");
+            Console.ReadLine();
+        }
+    }
+}
